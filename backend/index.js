e740f7cd02368d310df98971a43c8e039fe44854
Het Patel <106577127+Het01@users.noreply.github.com>
2023-08-03 23:13:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 5000;
 const mongoDB = require('./db');
 mongoDB();
 
-// app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin", "https://hp-electro-frontend.vercel.app");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// })
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"));
